561462206f168f0176ec561f2726c7a02151cf29
Torok Mark <torok.marko@gmail.com>
2019-10-23 11:26:23 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='esu',
-      version='1.0.5',
+      version='1.0.6',
       description='Enjoy the flexibility of structs with esu!',
       long_description=long_description,
       long_description_content_type='text/markdown',
